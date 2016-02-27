cee4a064640ea0b0adbfeffe6d84fd45a20672e4
fred <frederico.lopes.goncalves@gmail.com>
2016-02-27 17:17:45 +0100

    Removed unused resources from sample project
    Signed-off-by: fred <frederico.lopes.goncalves@gmail.com>

--- app/src/main/java/com/fred/inventory/MainActivity.java
+++ app/src/main/java/com/fred/inventory/MainActivity.java
@@ -1,31 +1,13 @@
 package com.fred.inventory;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import com.fred.inventory.data.outpan.models.Product;
-import com.fred.inventory.data.outpan.services.ProductService;
-import dagger.ObjectGraph;
-import javax.inject.Inject;
-import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-  @Bind(R.id.productId) EditText productId;
   @Bind(R.id.toolbar) Toolbar toolbar;
-  @Bind(R.id.fab) FloatingActionButton fab;
-  @Inject ProductService productService;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -33,60 +15,5 @@ public class MainActivity extends AppCompatActivity {
     ButterKnife.bind(this);
 
     setSupportActionBar(toolbar);
-
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null)
-            .show();
-      }
-    });
-
-    ObjectGraph.create(new RootModule()).plus(new ExperimentModule()).inject(this);
-  }
-
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
-  }
-
-  @OnClick(R.id.fetchDetailsButton) public void fetchProductData() {
-    String text = productId.getText().toString().trim();
-    if (text.isEmpty()) return;
-
-    productService.get(text)
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribeOn(Schedulers.io())
-        .subscribe(new Subscriber<Product>() {
-          @Override public void onCompleted() {
-
-          }
-
-          @Override public void onError(Throwable e) {
-            Toast.makeText(MainActivity.this, "Not found", Toast.LENGTH_SHORT).show();
-          }
-
-          @Override public void onNext(Product product) {
-            if (product.getName() == null || product.getName().isEmpty()) {
-              Toast.makeText(MainActivity.this, "FOUND!!! EMPTY THOUGH", Toast.LENGTH_SHORT).show();
-            } else {
-              Toast.makeText(MainActivity.this, product.getName(), Toast.LENGTH_SHORT).show();
-            }
-          }
-        });
   }
 }
