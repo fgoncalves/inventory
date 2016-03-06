65d98c22deaffcff154d53e4092ffe89f51af381
Fred <fgoncalves@babbel.com>
2016-03-06 19:11:36 +0100

    Added the missing list of supplies models to the database

---
+++ app/src/main/java/com/fred/inventory/data/db/models/ProductList.java
@@ -0,0 +1,20 @@
+package com.fred.inventory.data.db.models;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+
+/**
+ * This class represents a product list. Basically it's possible to have more than one list of
+ * supplies.
+ */
+public class ProductList extends RealmObject {
+  private RealmList<Product> products;
+
+  public RealmList<Product> getProducts() {
+    return products;
+  }
+
+  public void setProducts(RealmList<Product> products) {
+    this.products = products;
+  }
+}
